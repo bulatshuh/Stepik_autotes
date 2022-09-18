63300fb38ad4d0b2ad5f6c98d2c52ddab0bfc9b0
bulatshuh <bulat.khisamutdinov.00@mail.ru>
2022-09-18 18:00:36 +0300

    fixing login page test

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,10 +8,6 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
-        login_link.click()
-
     def should_be_login_url(self):
         cur_url = self.browser.current_url
         assert '/accounts/login/' in cur_url, 'URL address is wrong'
--- test_main_page.py
+++ test_main_page.py
@@ -5,10 +5,11 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = LoginPage(browser, link)
+    page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
